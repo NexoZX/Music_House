4fbcca097e77612a9efaa98aefbe54669f17948a
zxuru <cualquiercosa833@gmail.com>
2023-04-27 02:48:56 -0400

    Update footer.php

--- app/view/custom/footer.php
+++ app/view/custom/footer.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="css/nav.js"></script>
+<script type="text/javascript" src="<?php echo URL_PROJECT ?>/js/nav.js"></script>
 <script src="https://kit.fontawesome.com/87986eb156.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"></script>
